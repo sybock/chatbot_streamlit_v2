143dc6b5560ac75f3df3cb0352344377fbbb2ee7
sybock <68641631+sybock@users.noreply.github.com>
2021-06-04 13:44:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -204,7 +204,7 @@ def main():
         구어 대화체를 익히기 위해 먼저 한국어 Conversation Data로 학습하였고,<br> '이순재' 캐릭터를 모방하기 위해 거침없이 하이킥 대본으로 fine-tuning을 진행했습니다.<br>
         더 다채로운 챗봇 답변을 위해 DialoGPT (Zhang et al., 2019) 모델과 같은 방식으로 답변을 Re-Ranking하는 시스템을 구축했습니다. 
         또한, 자연스러운 답변을 유도하기 위해서 Scoring System을 개선하였고, 자동 응답을 출력하는 질문을 추가했습니다. <br><br>
-        국민 시트콤의 국민 할아버지, 거침없이 하아킥의 이순재와 대화를 해보세요!😁</p>
+        국민 시트콤의 국민 할아버지, 거침없이 하이킥의 이순재와 대화를 해보세요!😁</p>
         """
 
     display_app_header(main_txt,sub_txt,is_sidebar = False)
